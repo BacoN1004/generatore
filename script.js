c8c47dae8a4314cc0384d9219f3f670e2f7b2047
BacoN1004 <101983525+BacoN1004@users.noreply.github.com>
2022-10-09 17:12:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var randomNum = function(){
+    var random = Math.floor(Math.random()*23);
+    document.getElementById("num").innerHTML =random;
+}
+var socialJS = function(){
+    window.location = "https://bacon1004.github.io/BacoN-House/";
+}
